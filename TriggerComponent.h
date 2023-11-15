a2ed4051788779fe28988b285782cf11b1ab816c
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2023-11-15 10:43:00 +0500

    Update TriggerComponent.h
    LOC is decreased
    Trigger component created

--- TriggerComponent.h
+++ TriggerComponent.h
@@ -1,7 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Components/BoxComponent.h"
 #include "Mover.h"
@@ -25,7 +22,6 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 	UFUNCTION(BlueprintCallable)
 	void SetMover(UMover* Mover);
 
