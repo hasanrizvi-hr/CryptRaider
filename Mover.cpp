b8d05441d7f5b193caffe2088a722580df30c1e1
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-02-10 16:43:57 +0500

    Update Mover.cpp

--- Mover.cpp
+++ Mover.cpp
@@ -6,22 +6,18 @@ UMover::UMover()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 	// ...
 }
 // Called when the game starts
 void UMover::BeginPlay()
 {
 	Super::BeginPlay();
-
 	OrignalLocation = GetOwner()->GetActorLocation();
-	
 }
 // Called every frame
 void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	
 	FVector TargetLocation = OrignalLocation;
 	if (ShouldMove)
 	{
@@ -29,13 +25,10 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 	}
 		FVector CurrentLocation = GetOwner()->GetActorLocation();
 		float Speed = MoveOffset.Length() / MoveTime;
-		
 		FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
 		GetOwner()->SetActorLocation(NewLocation);
-	
 }
-
 void UMover::SetShouldMove(bool NewShouldMove)
 {
 	ShouldMove = NewShouldMove;
-} 
+}
